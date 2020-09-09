0914b45b900c395068e4a789ebacc132abeb876b
Kevin Staniszewski <kevingstanisz@gmail.com>
2020-09-09 17:31:11 -0500

    complete circular suffix

--- burrows/CircularSuffixArray.java
+++ burrows/CircularSuffixArray.java
@@ -4,26 +4,67 @@
  *  Description:
  **************************************************************************** */
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class CircularSuffixArray {
+
+    private String suffixString;
+    private Integer[] indexArr;
 
     // circular suffix array of s
     public CircularSuffixArray(String s) {
+        if (s == null) {
+            throw new IllegalArgumentException();
+        }
 
+        suffixString = s;
+        indexArr = new Integer[suffixString.length()];
+
+        for (int i = 0; i < suffixString.length(); i++) {
+            indexArr[i] = i;
+        }
+
+        Arrays.sort(indexArr, sortSuffix());
+    }
+
+    private Comparator<Integer> sortSuffix() {
+        return new SortSuffix();
+    }
+
+    private class SortSuffix implements Comparator<Integer> {
+        public int compare(Integer index1, Integer index2) {
+            int firstIndex, secondIndex, result = 0;
+
+            for (int i = 0; i < suffixString.length(); i++) {
+                firstIndex = (index1 + i) % suffixString.length();
+                secondIndex = (index1 + i) % suffixString.length();
+                result = suffixString.charAt(firstIndex) - suffixString.charAt(secondIndex);
+                if (result != 0) {
+                    return result;
+                }
+            }
+
+            return 0;
+        }
     }
 
     // length of s
     public int length() {
-        return 0;
+        return suffixString.length();
     }
 
     // returns index of ith sorted suffix
     public int index(int i) {
-        return 0;
+        if ((i < 0) || (i >= suffixString.length())) {
+            throw new IllegalArgumentException();
+        }
+
+        return indexArr[i];
     }
 
     // unit testing (required)
     public static void main(String[] args) {
 
     }
-
 }
